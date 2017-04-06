0c2557047d72082ed37b6903dba09cb837d56dd1
luis.perez <analista.guma@gmail.com>
2017-04-06 17:41:16 -0600

    jue 06 Abr 17 545pm

--- app/src/main/java/com/guma/desarrollo/gmv/Adapters/CustomArrayAdapter.java
+++ app/src/main/java/com/guma/desarrollo/gmv/Adapters/CustomArrayAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 //import com.danielme.blog.demo.listviewcheckbox.R;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -39,6 +40,7 @@ public class CustomArrayAdapter extends ArrayAdapter<Row> implements View.OnClic
             convertView = layoutInflater.inflate(R.layout.listview_row, parent, false);
             holder.setTextViewTitle((TextView) convertView.findViewById(R.id.textViewTitle));
             holder.setTextViewSubtitle((TextView) convertView.findViewById(R.id.textViewSubtitle));
+            holder.setTextViewSubSubTitle((TextView) convertView.findViewById(R.id.textViewSubSubtitle));
             holder.setCheckBox((CheckBox) convertView.findViewById(R.id.checkBox));
             convertView.setTag(holder);
         }
@@ -49,9 +51,11 @@ public class CustomArrayAdapter extends ArrayAdapter<Row> implements View.OnClic
         final Row row = getItem(position);
         holder.getTextViewTitle().setText(row.getTitle());
         holder.getTextViewSubtitle().setText(row.getSubtitle());
+        holder.getTextViewSubSubTitle().setText(row.getSubsubtitle());
         holder.getCheckBox().setTag(position);
         holder.getCheckBox().setChecked(row.isChecked());
         holder.getCheckBox().setOnClickListener(this);
+        changeBackground(CustomArrayAdapter.this.getContext(),holder.getCheckBox());
         return convertView;
     }
 
@@ -60,14 +64,28 @@ public class CustomArrayAdapter extends ArrayAdapter<Row> implements View.OnClic
         CheckBox checkBox = (CheckBox) v;
         int position = (Integer) v.getTag();
         getItem(position).setChecked(checkBox.isChecked());
-
+        changeBackground(CustomArrayAdapter.this.getContext(), checkBox);
         //String msg = this.getContext().getString(R.string.check_toast, position, checkBox.isChecked());
         //Toast.makeText(this.getContext(), msg, Toast.LENGTH_SHORT).show();;
     }
 
+    private void changeBackground(Context context, CheckBox checkBox) {
+        View row = (View) checkBox.getParent();
+        Drawable drawable = context.getResources().getDrawable(
+                R.drawable.listview_selector_checked);
+        if (checkBox.isChecked()) {
+            drawable = context.getResources().getDrawable(
+                    R.drawable.listview_selector_checked);
+        } else {
+            drawable = context.getResources().getDrawable(
+                    R.drawable.listview_selector);
+        }
+        row.setBackgroundDrawable(drawable);
+    }
     static class Holder {
         TextView textViewTitle;
         TextView textViewSubtitle;
+        TextView textViewSubSubTitle;
         CheckBox checkBox;
 
         public TextView getTextViewTitle()
@@ -89,6 +107,15 @@ public class CustomArrayAdapter extends ArrayAdapter<Row> implements View.OnClic
         {
             this.textViewSubtitle = textViewSubtitle;
         }
+
+        public TextView getTextViewSubSubTitle() {
+            return textViewSubSubTitle;
+        }
+
+        public void setTextViewSubSubTitle(TextView textViewSubSubTitle) {
+            this.textViewSubSubTitle = textViewSubSubTitle;
+        }
+
         public CheckBox getCheckBox()
         {
             return checkBox;
